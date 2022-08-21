49d7665ab1e278f3c581d63bdc5c5d224d358cd8
Judson Fabio <104886613+Judsonofc@users.noreply.github.com>
2022-08-21 16:10:18 -0300

    Initial commit

---
+++ app/src/main/java/com/judson10ofc/testfirebase/MainActivity.java
@@ -0,0 +1,30 @@
+package com.judson10ofc.testfirebase;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+    private DatabaseReference reference;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        reference = FirebaseDatabase.getInstance().getReference().child("usuarios");
+        reference.child("usuarios").child("001").setValue("judson10");
+
+    }
+
+}
---
+++ app/src/main/java/com/judson10ofc/testfirebase/User.java
@@ -0,0 +1,34 @@
+package com.judson10ofc.testfirebase;
+
+public class User {
+    String nome;
+    String sobrenome;
+    int idade;
+
+    public User() {
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+}
